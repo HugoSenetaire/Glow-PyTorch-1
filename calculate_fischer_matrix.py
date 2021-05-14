4c2b4a907224ca88ae640987997e6d0bb072fafb
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2021-05-14 16:51:54 +0200

    test

--- calculate_fischer_matrix.py
+++ calculate_fischer_matrix.py
@@ -116,7 +116,7 @@ def gradient_mean_from_model(model, sampling_dataset , T = 1000):
         for _, param in model.named_parameters():
             # if param.grad is not None and not torch.isinf(param.grad).any() and not torch.isnan(param.grad).any() :
                 # if torch.isinf(param.grad).any():
-            # current_grad.append(-param.grad.view(-1))
+            current_grad.append(-param.grad.view(-1))
 
         current_grad = torch.cat(current_grad)
         if torch.isinf(current_grad).any() or torch.isnan(current_grad).any() :
