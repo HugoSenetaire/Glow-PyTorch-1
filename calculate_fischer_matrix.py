369dc8944dca612a528166df804ee35941fac8ab
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2021-05-15 15:40:03 +0200

    test

--- calculate_fischer_matrix.py
+++ calculate_fischer_matrix.py
@@ -135,9 +135,9 @@ def gradient_mean_from_model(model, sampling_dataset , T = 1000):
         else :
             total_grad =  (n/n+1)  * total_grad + (1/n+1) * current_grad
         
-        print("Total grad", total_grad[indexNan])
-        print("Total grad_aux", total_grad_aux[indexNan])
-        print("current grad", current_grad[indexNan])
+        print("Total grad", total_grad)
+        print("Total grad_aux", total_grad_aux)
+        print("current grad", current_grad)
 
         # if torch.isinf(total_grad).any() or torch.isnan(total_grad).any() :
         #     print("Total Grad is inf")
